06e8645864e9fb31c05976610c883d2d7e77c79a
stijnrosaer <stijn.rosaer@student.uantwerpen.be>
2018-11-16 16:18:29 +0100

    clean start

--- Game.cpp
+++ Game.cpp
@@ -5,18 +5,5 @@
 #include "Game.h"
 
 Game::Game() {
-    //sf::RenderWindow window(sf::VideoMode(width, height), "Roadfighter");
 
-    /*
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.display();
-    }
-     */
 }
--- Game.h
+++ Game.h
@@ -9,13 +9,6 @@
 #include "PlayerCarSFML.h"
 
 class Game {
-private:
-    shared_ptr<roadfighterSFML::PlayerCar> player;
-    const int width = 800;
-    const int height = 600;
-public:
-    Game();
-
 
 };
 
