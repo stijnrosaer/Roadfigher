8159977e73a85090a46ec047a5ad4c1e6faa76ed
stijnrosaer <stijn.rosaer@student.uantwerpen.be>
2018-11-16 16:23:43 +0100

    create window in game

--- Game.h
+++ Game.h
@@ -9,6 +9,13 @@
 #include "PlayerCarSFML.h"
 
 class Game {
+private:
+    shared_ptr<roadfighterSFML::PlayerCar> player;
+    const int width = 800;
+    const int height = 600;
+public:
+    Game();
+
 
 };
 
