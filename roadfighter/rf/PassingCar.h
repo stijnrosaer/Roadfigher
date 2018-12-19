db8bcffe2709d97f291a1d9b7d3913babecd4a52
stijnrosaer <stijn.rosaer@student.uantwerpen.be>
2018-12-19 10:28:50 +0100

    toevoegen boundaries in alle entities

--- roadfighter/rf/PassingCar.h
+++ roadfighter/rf/PassingCar.h
@@ -9,6 +9,13 @@
 
 namespace roadfighter {
     class PassingCar : public roadfighter::Entity {
+        struct boundaries{
+            pair<float, float> loc;
+            float width;
+            float height;
+            pair<float, float> tlLoc;
+            pair<float, float> brLoc;
+        };
     protected:
         pair<float, float> loc;
         float width;
